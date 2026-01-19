eb70447e51fc0019d240bfcec25ada3a8bacb9c7
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-18 22:36:28 -0300

    Update gabaritos.js

--- gabaritos.js
+++ gabaritos.js
@@ -1,6 +1,7 @@
-
-<script src="gabaritos.js"></script>
-<script>
-   // Aqui você coloca a lógica para mostrar o gabarito na tela
-   // Exemplo: document.getElementById('resultado').innerText = dbGabaritos.rt[1];
-</script>
+const dbGabaritos = {
+    'rt': { 
+        1: `1.2. EXERCÍCIO I DE RECLAMAÇÃO TRABALHISTA ()
+        ... (todo o seu texto aqui) ...
+        2 - PEDIDOS E REQUERIMENTOS FINAIS...`
+    }
+};
