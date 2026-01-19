f1663685230ab75fe7dcf9b3a52a49ea7acb9384
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-18 23:27:58 -0300

    Update gabaritos.js

--- gabaritos.js
+++ gabaritos.js
@@ -10,7 +10,8 @@ e 8.000 calorias.
 Relata que foi contratado no mesmo dia em que Juan para exercer a mesma
 função, na mesma filial. O trabalho se dava com a mesma perfeição técnica e
 produtividade, porém Juan recebia salário fixo de R$ 2.500,00 por mês.
-
+P á g i n a 9 | 551
+ @aryannalinhares @professoraaryannalinhares
 O senhor Fininho conta que, em janeiro do ano da extinção do contrato, faltou
 ao trabalho por um dia para comparecer em juízo como parte no processo em que
 litigava contra seu ex-empregador. Embora tenha apresentado a certidão da
@@ -21,18 +22,122 @@ a sexta-feira e usufruía de apenas 30 minutos de intervalo intrajornada.
 No curso do contrato, o empregador depositava apenas 4% do valor da
 remuneração a título de FGTS, pois havia acordo coletivo de trabalho autorizando
 o recolhimento de apenas metade do valor.
-
+Na qualidade de advogado(a) do reclamante, apresente a medida
+processual cabível para a defesa de seus direitos.
+Nos casos em que a lei exigir liquidação de valores, não se faz necessária
+sua apresentação pelo Examinando, pois admite-se que o escritório possui setor
+próprio ou contratado especificamente para tal fim. (Valor: 5,00)
 RESOLUÇÃO
-AO DOUTO JUÍZO DA ... VARA DO TRABALHO DE FLORIANÓPOLIS/SC
+AO DOUTO JUÍZO DA ... VARA DO TRABALHO DE
+FLORIANÓPOLIS/SC
 JOSÉ FININHO, garçom, qualificação e endereço completos, vem,
 respeitosamente, perante Vossa Excelência, por intermédio de seu
 advogado adiante assinado (procuração anexa), com escritório
 profissional no endereço completo, onde recebe intimações ou
-notificações, com fulcro no art. 840, caput e § 1o, da CLT, PROPOR:
-RECLAMAÇÃO TRABALHISTA, pelo rito (...) em face de HEART ATTACK GRILL LTDA.
-...
+notificações, com fulcro no art. 840, caput e § 1º, da CLT, PROPOR:
+RECLAMAÇÃO TRABALHISTA, pelo rito (...)
+em face de HEART ATTACK GRILL LTDA., qualificação e endereço
+completos, e SINDICATO DOS EMPREGADOS ..., qualificação e
+P á g i n a 10 | 551
+ @aryannalinhares @professoraaryannalinhares
+endereço completos, pelas razões de fato e de direito a seguir expostas.
+I – MÉRITO
+1. Equiparação salarial
+O reclamante foi contratado pela reclamada no mesmo dia em que Juan
+para exercer a mesma função, na mesma filial, com a mesma
+produtividade e perfeição técnica, porém, enquanto recebia R$ 2.000,00,
+o seu colega recebia salário fixo de R$ 2.500,00 por mês.
+Nos termos do art. 461, caput e § 1º, da CLT, é devido o mesmo salário
+aos empregados do mesmo empregador que exerçam a mesma função,
+no mesmo estabelecimento comercial, com a mesma produtividade e
+perfeição técnica e cuja diferença de tempo de serviço para o mesmo
+empregador não seja superior a quatro anos e de tempo na função não
+seja superior a dois anos.
+Diante do exposto, requer a condenação da reclamada ao pagamento
+das diferenças salariais, bem como de seus reflexos nas verbas
+contratuais e rescisórias (aviso prévio, décimo terceiro, férias acrescidas
+de 1/3 e FGTS - depósitos e multa de 40%).
+Por fim, requer a retificação da CTPS do empregado para constar o seu
+real salário, no valor e R$ 2.500,00, nos termos do art. 29, § 1º, da CLT.
+R$
+2. Devolução do desconto
+A reclamada descontou do salário do reclamante um dia de trabalho no
+mês de janeiro do ano da extinção do contrato, em razão de o reclamante
+ter faltado ao trabalho para comparecer em juízo como parte no processo
+em que litigava contra seu antigo empregador, muito embora tivesse
+apresentado certidão da Justiça do Trabalho confirmando suas
+alegações.
+P á g i n a 11 | 551
+ @aryannalinhares @professoraaryannalinhares
+Com base no art. 473, VIII, da CLT e súmula 155, TST, o empregado
+poderá deixar de comparecer ao serviço para comparecimento como
+parte na Justiça do Trabalho.
+Diante do exposto, requer a condenação da reclamada à devolução do
+dia de trabalho descontado de seu salário. R$
+3. Intervalo intrajornada
+O reclamante trabalhava 8 horas diárias de segunda a sexta-feira e
+usufruía de apenas 30 minutos de intervalo intrajornada.
+Nos termos do art. 71, caput, da CLT, aqueles que laboram mais de 6
+horas diárias fazem jus a um intervalo intrajornada de, no mínimo, 1 hora,
+o qual não era observado.
+Diante do exposto, requer a condenação da reclamada ao pagamento do
+período suprimido, ou seja, de 30 minutos diários, acrescidos de 50%, à
+luz do art. 71, § 4º, da CLT. R$
+4. Diferenças de FGTS
+No curso do contrato, o empregador depositava apenas 4% do valor da
+remuneração do reclamante a título de FGTS, pois havia acordo coletivo
+de trabalho autorizando o recolhimento de apenas metade do valor.
+Nos termos do art. 611-B, III, da CLT, é ilícita e, portanto, nula a cláusula
+de acordo coletivo de trabalho que implique redução do valor dos
+depósitos mensais do FGTS. Ressalte-se que, o art. 15 da Lei nº
+8.036/90 determina que os depósitos do FGTS devem ser de 8% da
+remuneração paga ou devida ao trabalhador no mês anterior.
+Diante do exposto, requer a declaração de nulidade da cláusula do
+acordo coletivo de trabalho que estabelece a redução dos depósitos do
+FGTS e a condenação da reclamada ao pagamento das diferenças
+salariais. R$
+P á g i n a 12 | 551
+ @aryannalinhares @professoraaryannalinhares
+5. Honorários advocatícios
+Requer a condenação da reclamada ao pagamento de honorários
+advocatícios, no importe de 15%, sobre o valor que resultar da liquidação,
+à luz do art. 791-A da CLT. R$
+II – PEDIDOS
+Diante do exposto, requer:
+a) a condenação da reclamada ao pagamento das diferenças salariais,
+bem como de seus reflexos nas verbas contratuais e rescisórias
+R$ ............................................................................. ;
+b) a condenação da reclamada à devolução do dia de trabalho
+descontado de seu salário ................................. R$ ....;
+c) a condenação da reclamada ao pagamento do período suprimido, ou
+seja, de 30 minutos diários, acrescidos de 50%, nos termos do art.
+71, § 4º, da CLT ............................................. R$....;
+d) a declaração de nulidade da cláusula do acordo coletivo de trabalho
+que institui a redução dos depósitos do FGTS e a condenação da
+reclamada ao pagamento das diferenças salariais;
+e) a condenação da reclamada ao pagamento de honorários
+advocatícios, no importe de 15% sobre o valor que resultar da
+liquidação, à luz do art. 791-A da CLT.
+III – REQUERIMENTOS FINAIS
+Diante do exposto, requer:
+a) a notificação da Reclamada e do Sindicato dos Empregados em...,
+para oferecer resposta à reclamação trabalhista, sob pena de revelia
+e confissão quanto à matéria de fato;
+b) a produção de todos os meios de prova em direito admitidos, em
+especial a prova documental, o depoimento pessoal e a oitiva de
+P á g i n a 13 | 551
+ @aryannalinhares @professoraaryannalinhares
+testemunhas; e
+c) por fim, a procedência dos pedidos com a condenação da
+reclamada ao pagamento das verbas pleiteadas, acrescidas de
+juros e correção monetária.
+Atribui-se à causa o valor de R$... .
+Nestes termos,
+pede deferimento.
+Local e data.
 Advogado(a)
-OAB no`
+OAB nº
+`
     }, 
 
     'consigna': { 
@@ -48,11 +153,59 @@ AÇÃO DE CONSIGNAÇÃO EM PAGAMENTO
 em face de JOANA FIRMINO, brasileira, casada, costureira, residente na
 Rua Lopes Andrade, 20, Maceió/AL, CEP: 10.0001-00, pelas razões de
 fato e de direito a seguir expostas.
-
-... (texto completo da consignação que você enviou) ...
-
+I – FATOS
+A consignatária foi contratada por Zenga Modas Ltda., em 12.09.2008,
+para exercer a função de costureira, na unidade de Maceió/AL, sendo
+dispensada sem justa causa em 11.10.2012, mediante aviso-prévio
+indenizado. Em seu armário, foi encontrado um telefone celular de sua
+propriedade. Joana foi cientificada de que, no dia 15.10.2012, às 10h,
+deveria comparecer na empresa para receber suas verbas rescisórias.
+Contudo, na data e hora designadas, a empregada não compareceu.
+A consignante tem o intuito de pagar as verbas devidas à consignatária,
+razão pela qual propôs a presente medida processual.
+II – MÉRITO
+Em razão da extinção do contrato de trabalho sem justa causa, são
+devidas à consignatária as seguintes verbas, no importe de R$ ..., as
+quais a consignante pretende depositar com o objetivo de que seja
+P á g i n a 112 | 551
+ @aryannalinhares @professoraaryannalinhares
+declarada extinta a obrigação:
+a) Saldo de salário (11 dias)...............................R$.............
+b) Aviso-prévio (42 dias) ....................................R$.............
+c) 13º salário proporcional (11/12)......................R$.............
+d) Férias em dobro – 2010/2011 + 1/3 ...............R$.............
+e) Férias + 1/3 (2011/2012)................................R$.............
+f) Férias proporcionais (2/12) + 1/3 ....................R$.............
+g) Multa de 40% do FGTS..................................R$.............
+Total devido .......................................................R$.............
+Requer, ainda, a devolução do aparelho celular e a entrega da CTPS,
+nos moldes do art. 542, I, do CPC.
+Postula a consignante o depósito das verbas, do celular e da CTPS,
+visando à extinção da obrigação, nos termos do art. 546 do CPC.
+Postula também o depósito dos documentos que comprovam a
+comunicação da extinção do contrato de trabalho aos órgãos
+competentes para acesso da consignatária ao seguro-desemprego e ao
+FGTS.
+Ressalte-se que, por não haver atraso no pagamento das verbas
+rescisórias, é indevida a multa prevista no art. 477, § 8º, da CLT.
+III – REQUERIMENTOS FINAIS
+Diante do exposto, requer:
+a) o deferimento do depósito dos valores referidos, do aparelho celular e
+da CTPS (art. 542, I, do CPC);
+b) a citação da consignatária para levantar os valores, o aparelho celular
+e a CTPS depositados com efeito de quitação ou oferecer resposta, sob
+pena de revelia e declaração da extinção da obrigação (art. 542, II, do
+CPC);
+P á g i n a 113 | 551
+ @aryannalinhares @professoraaryannalinhares
+c) a produção de todos os meios de prova em direito admitidos, em
+especial a prova documental;
+d) a procedência do pedido com a declaração de extinção da obrigação
+e condenação da consignatária ao pagamento de custas e honorários
+advocatícios no importe 15%, com base no art. 791-A da CLT.
 Atribui-se à causa o valor de R$....
-Nestes termos, pede deferimento.
+Nestes termos,
+pede deferimento.
 Local e data.
 Advogado
 OAB no`,
