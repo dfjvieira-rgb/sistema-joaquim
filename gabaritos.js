08e0d6f8889c28bb2d093f0c8e0b7561d7deb9a8
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-18 22:27:53 -0300

    Update gabaritos.js

--- gabaritos.js
+++ gabaritos.js
@@ -1,1 +1,6 @@
 
+<script src="gabaritos.js"></script>
+<script>
+   // Aqui você coloca a lógica para mostrar o gabarito na tela
+   // Exemplo: document.getElementById('resultado').innerText = dbGabaritos.rt[1];
+</script>
