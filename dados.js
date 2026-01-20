a3150f41be86111813cb10e713bfbf75dcd5cc5b
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-20 08:47:15 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,38 +1,132 @@
 // Use VAR para permitir que o arquivo principal acesse os dados
 var dbGabaritosExternos = {
     'rt': { 
-        1: `1.2. EXERCÍCIO I DE RECLAMAÇÃO TRABALHISTA ()
+        1: `1.2. EXERCÍCIO I DE RECLAMAÇÃO TRABALHISTA
+José Fininho foi contratado pela empresa Heart Attack Grill Ltda., para
+trabalhar na cidade de Florianópolis/SC, como garçom, mediante salário de
+R$ 2.000,00.
+O empregado afirma que uma das especialidades da Lanchonete era o
+sanduíche denominado quadruple bypass com 4 hambúrgueres: 1 quilo de carne
+e 8.000 calorias.
+Relata que foi contratado no mesmo dia em que Juan para exercer a mesma
+função, na mesma filial. O trabalho se dava com a mesma perfeição técnica e
+produtividade, porém Juan recebia salário fixo de R$ 2.500,00 por mês.
+O senhor Fininho conta que, em janeiro do ano da extinção do contrato, faltou
+ao trabalho por um dia para comparecer em juízo como parte no processo em que
+litigava contra seu ex-empregador. Embora tenha apresentado a certidão da
+Justiça do Trabalho confirmando suas alegações, o dia foi descontado do seu
+salário.
+O empregado comenta também que trabalhava 8 horas diárias de segunda
+a sexta-feira e usufruía de apenas 30 minutos de intervalo intrajornada.
+No curso do contrato, o empregador depositava apenas 4% do valor da
+remuneração a título de FGTS, pois havia acordo coletivo de trabalho autorizando
+o recolhimento de apenas metade do valor.
+Na qualidade de advogado(a) do reclamante, apresente a medida
+processual cabível para a defesa de seus direitos.
+Nos casos em que a lei exigir liquidação de valores, não se faz necessária
+sua apresentação pelo Examinando, pois admite-se que o escritório possui setor
+próprio ou contratado especificamente para tal fim. (Valor: 5,00)
 
-José Fininho foi contratado pela empresa Heart Attack Grill Ltda., para trabalhar na cidade de Florianópolis/SC, como garçom, mediante salário de R$ 1.500,00.
-
-O empregado afirma que uma das especialidades da Lanchonete era o sanduíche denominado quadruple bypass com 4 hambúrgueres: 1 quilo de carne e 8.000 calorias.
-
-Relata que foi contratado no mesmo dia em que Juan para exercer a mesma função, na mesma filial. O trabalho se dava com a mesma perfeição técnica e produtividade, porém Juan recebia salário fixo de R$ 2.500,00 por mês.
-
-O senhor Fininho conta que, em janeiro do ano da extinção do contrato, faltou ao trabalho por um dia para comparecer em juízo como parte no processo em que litigava contra seu ex-empregador. Embora tenha apresentado a certidão da Justiça do Trabalho confirmando suas alegações, o dia foi descontado do seu salário.
-
-O empregado comenta também que trabalhava 8 horas diárias de segunda a sexta-feira e usufruía de apenas 30 minutos de intervalo intrajornada.
-
-No curso do contrato, o empregador depositava apenas 4% do valor da remuneração a título de FGTS, pois havia acordo coletivo de trabalho autorizando o recolhimento de apenas metade do valor.
-
-RESOLUÇÃO (pág. 9 a 11)
-
-AO DOUTO JUÍZO DA ... VARA DO TRABALHO DE FLORIANÓPOLIS/SC
-
-JOSÉ FININHO, garçom, qualificação e endereço completos, vem, respeitosamente, perante Vossa Excelência, por intermédio de seu advogado adiante assinado (procuração anexa), com escritório profissional no endereço completo, onde recebe intimações ou notificações, com fulcro no art. 840, caput e § 1º, da CLT, PROPOR:
-
-RECLAMAÇÃO TRABALHISTA, pelo rito (...) em face de HEART ATTACK GRILL LTDA., qualificação e endereço completos.
-
-1 - MÉRITO
-
-1. Equiparação salarial: Requer a condenação ao pagamento das diferenças salariais nos termos do art. 461 da CLT.
-
-2. Devolução do desconto: Base legal art. 473, VIII, da CLT e súmula 155, TST.
-
-3. Intervalo intrajornada: Requer pagamento de 30 min com adicional de 50% (art. 71, § 4º, CLT).
-
-4. Diferenças de FGTS: Cláusula nula conforme art. 611-B, III, da CLT.
-
-2 - PEDIDOS E REQUERIMENTOS FINAIS...`
+RESOLUÇÃO
+AO DOUTO JUÍZO DA ... VARA DO TRABALHO DE
+FLORIANÓPOLIS/SC
+JOSÉ FININHO, garçom, qualificação e endereço completos, vem,
+respeitosamente, perante Vossa Excelência, por intermédio de seu
+advogado adiante assinado (procuração anexa), com escritório
+profissional no endereço completo, onde recebe intimações ou
+notificações, com fulcro no art. 840, caput e § 1º, da CLT, PROPOR:
+RECLAMAÇÃO TRABALHISTA, pelo rito (...)
+em face de HEART ATTACK GRILL LTDA., qualificação e endereço
+completos, e SINDICATO DOS EMPREGADOS ..., qualificação e endereço completos, pelas razões de fato e de direito a seguir expostas.
+I – MÉRITO
+1. Equiparação salarial
+O reclamante foi contratado pela reclamada no mesmo dia em que Juan
+para exercer a mesma função, na mesma filial, com a mesma
+produtividade e perfeição técnica, porém, enquanto recebia R$ 2.000,00,
+o seu colega recebia salário fixo de R$ 2.500,00 por mês.
+Nos termos do art. 461, caput e § 1º, da CLT, é devido o mesmo salário
+aos empregados do mesmo empregador que exerçam a mesma função,
+no mesmo estabelecimento comercial, com a mesma produtividade e
+perfeição técnica e cuja diferença de tempo de serviço para o mesmo
+empregador não seja superior a quatro anos e de tempo na função não
+seja superior a dois anos.
+Diante do exposto, requer a condenação da reclamada ao pagamento
+das diferenças salariais, bem como de seus reflexos nas verbas
+contratuais e rescisórias (aviso prévio, décimo terceiro, férias acrescidas
+de 1/3 e FGTS - depósitos e multa de 40%).
+Por fim, requer a retificação da CTPS do empregado para constar o seu
+real salário, no valor e R$ 2.500,00, nos termos do art. 29, § 1º, da CLT.
+R$
+2. Devolução do desconto
+A reclamada descontou do salário do reclamante um dia de trabalho no
+mês de janeiro do ano da extinção do contrato, em razão de o reclamante
+ter faltado ao trabalho para comparecer em juízo como parte no processo
+em que litigava contra seu antigo empregador, muito embora tivesse
+apresentado certidão da Justiça do Trabalho confirmando suas
+alegações.
+Com base no art. 473, VIII, da CLT e súmula 155, TST, o empregado
+poderá deixar de comparecer ao serviço para comparecimento como
+parte na Justiça do Trabalho.
+Diante do exposto, requer a condenação da reclamada à devolução do
+dia de trabalho descontado de seu salário. R$
+3. Intervalo intrajornada
+O reclamante trabalhava 8 horas diárias de segunda a sexta-feira e
+usufruía de apenas 30 minutos de intervalo intrajornada.
+Nos termos do art. 71, caput, da CLT, aqueles que laboram mais de 6
+horas diárias fazem jus a um intervalo intrajornada de, no mínimo, 1 hora,
+o qual não era observado.
+Diante do exposto, requer a condenação da reclamada ao pagamento do
+período suprimido, ou seja, de 30 minutos diários, acrescidos de 50%, à
+luz do art. 71, § 4º, da CLT. R$
+4. Diferenças de FGTS
+No curso do contrato, o empregador depositava apenas 4% do valor da
+remuneração do reclamante a título de FGTS, pois havia acordo coletivo
+de trabalho autorizando o recolhimento de apenas metade do valor.
+Nos termos do art. 611-B, III, da CLT, é ilícita e, portanto, nula a cláusula
+de acordo coletivo de trabalho que implique redução do valor dos
+depósitos mensais do FGTS. Ressalte-se que, o art. 15 da Lei nº
+8.036/90 determina que os depósitos do FGTS devem ser de 8% da
+remuneração paga ou devida ao trabalhador no mês anterior.
+Diante do exposto, requer a declaração de nulidade da cláusula do
+acordo coletivo de trabalho que estabelece a redução dos depósitos do
+FGTS e a condenação da reclamada ao pagamento das diferenças
+salariais. R$
+5. Honorários advocatícios
+Requer a condenação da reclamada ao pagamento de honorários
+advocatícios, no importe de 15%, sobre o valor que resultar da liquidação,
+à luz do art. 791-A da CLT. R$
+II – PEDIDOS
+Diante do exposto, requer:
+a) a condenação da reclamada ao pagamento das diferenças salariais,
+bem como de seus reflexos nas verbas contratuais e rescisórias
+R$ ............................................................................. ;
+b) a condenação da reclamada à devolução do dia de trabalho
+descontado de seu salário ................................. R$ .... ;
+c) a condenação da reclamada ao pagamento do período suprimido, ou
+seja, de 30 minutos diários, acrescidos de 50%, nos termos do art.
+71, § 4º, da CLT ............................................. R$.... ;
+d) a declaração de nulidade da cláusula do acordo coletivo de trabalho
+que institui a redução dos depósitos do FGTS e a condenação da
+reclamada ao pagamento das diferenças salariais;
+e) a condenação da reclamada ao pagamento de honorários
+advocatícios, no importe de 15% sobre o valor que resultar da
+liquidação, à luz do art. 791-A da CLT.
+III – REQUERIMENTOS FINAIS
+Diante do exposto, requer:
+a) a notificação da Reclamada e do Sindicato dos Empregados em...,
+para oferecer resposta à reclamação trabalhista, sob pena de revelia
+e confissão quanto à matéria de fato;
+b) a produção de todos os meios de prova em direito admitidos, em
+especial a prova documental, o depoimento pessoal e a oitiva de testemunhas; e
+c) por fim, a procedência dos pedidos com a condenação da
+reclamada ao pagamento das verbas pleiteadas, acrescidas de
+juros e correção monetária.
+Atribui-se à causa o valor de R$... .
+Nestes termos,
+pede deferimento.
+Local e data.
+Advogado(a)
+OAB nº`
     }
 };
+
