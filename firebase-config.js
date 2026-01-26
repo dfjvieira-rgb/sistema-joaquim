6e75187723ddc737b791e80b5252967af3a5e69d
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-26 12:32:08 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,22 @@
+// firebase-config.js - Conexão Centralizada
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+// COLE ABAIXO AS SUAS CREDENCIAIS DO FIREBASE CONSOLE
 const firebaseConfig = {
-    apiKey: "AIzaSyAmigODFK8R9c0-fWtagdxLWu9xkODfKYQ",
-    authDomain: "masteroab-db5e1.firebaseapp.com",
-    projectId: "masteroab-db5e1",
-    databaseURL: "https://masteroab-db5e1-default-rtdb.firebaseio.com"
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    databaseURL: "https://SEU_PROJETO.firebaseio.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_ID",
+    appId: "SEU_APP_ID"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta as instâncias para uso nos outros arquivos
 export const db = getDatabase(app);
+export const auth = getAuth(app);
