c5e4acbc25f5b5084cfa7a4b785df8bad1614660
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-26 15:05:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-self.addEventListener('fetch', function(event) {});
+// sw.js - Service Worker (Capacidade Offline)
+const CACHE_NAME = 'aryanna-master-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './mentoria.html',
+  './estilos.css',
+  './app-master.js',
+  './estruturas.js',
+  './ui-components.js',
+  './pdf-engine.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
