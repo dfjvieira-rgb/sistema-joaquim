f2f5d0966d737d91f1043e152509e39307c68de4
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-25 22:37:25 -0300

    Update dados_espelhos.js

--- dados_espelhos.js
+++ dados_espelhos.js
@@ -1,6 +1,28 @@
-// Arquivo: dados_espelhos.js
 window.MEUS_ESPELHOS = {
-  "44": `... todo o HTML do 44 que já fizemos ...`,
-  "43": `... o HTML do 43 que você vai criar ...`,
-  "42": `... e assim por diante ...`
+    "44": `
+        <div class="nota-floating">ESPELHO DE CORREÇÃO - EXAME 44</div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.10" onchange="window.calcNota()">
+            <div class="espelho-txt">Endereçamento ao Juízo da ... Vara do Trabalho de ...</div>
+            <div class="espelho-pt">0,10</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.40" onchange="window.calcNota()">
+            <div class="espelho-txt">Qualificação das partes e indicação do Art. 840, § 1º, da CLT</div>
+            <div class="espelho-pt">0,40</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.50" onchange="window.calcNota()">
+            <div class="espelho-txt">Tese de Mérito 01 (Exemplo: Verbas Rescisórias)</div>
+            <div class="espelho-pt">0,50</div>
+        </div>
+    `,
+    "43": `
+        <div class="nota-floating">ESPELHO DE CORREÇÃO - EXAME 43</div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.10" onchange="window.calcNota()">
+            <div class="espelho-txt">Conteúdo específico do Exame 43...</div>
+            <div class="espelho-pt">0,10</div>
+        </div>
+    `
 };
