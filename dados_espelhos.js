a7febfbeeaeb151dbd63be34c8ed3d920a0b8f4d
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-25 23:01:30 -0300

    Update dados_espelhos.js

--- dados_espelhos.js
+++ dados_espelhos.js
@@ -1,28 +1,27 @@
 window.MEUS_ESPELHOS = {
-    "44": `
-        <div class="nota-floating">ESPELHO DE CORREÇÃO - EXAME 44</div>
-        <div class="espelho-item">
-            <input type="checkbox" value="0.10" onchange="window.calcNota()">
-            <div class="espelho-txt">Endereçamento ao Juízo da ... Vara do Trabalho de ...</div>
-            <div class="espelho-pt">0,10</div>
-        </div>
-        <div class="espelho-item">
-            <input type="checkbox" value="0.40" onchange="window.calcNota()">
-            <div class="espelho-txt">Qualificação das partes e indicação do Art. 840, § 1º, da CLT</div>
-            <div class="espelho-pt">0,40</div>
-        </div>
-        <div class="espelho-item">
-            <input type="checkbox" value="0.50" onchange="window.calcNota()">
-            <div class="espelho-txt">Tese de Mérito 01 (Exemplo: Verbas Rescisórias)</div>
-            <div class="espelho-pt">0,50</div>
-        </div>
-    `,
-    "43": `
-        <div class="nota-floating">ESPELHO DE CORREÇÃO - EXAME 43</div>
-        <div class="espelho-item">
-            <input type="checkbox" value="0.10" onchange="window.calcNota()">
-            <div class="espelho-txt">Conteúdo específico do Exame 43...</div>
-            <div class="espelho-pt">0,10</div>
-        </div>
-    `
+  "44": `
+    <div class="espelho-group-title">PEÇA PRÁTICO-PROFISSIONAL</div>
+    <div class="espelho-item">
+        <input type="checkbox" value="0.10" onchange="window.calcNota()">
+        <div class="espelho-txt">Endereçamento ao Juízo da ... Vara do Trabalho de ...</div>
+        <div class="espelho-pt">0,10</div>
+    </div>
+    <div class="espelho-item">
+        <input type="checkbox" value="0.40" onchange="window.calcNota()">
+        <div class="espelho-txt">Qualificação das partes e indicação do Art. 840, § 1º, da CLT</div>
+        <div class="espelho-pt">0,40</div>
+    </div>
+    <div class="espelho-item">
+        <input type="checkbox" value="0.50" onchange="window.calcNota()">
+        <div class="espelho-txt">Tese de Mérito 01 (Exemplo: Verbas Rescisórias)</div>
+        <div class="espelho-pt">0,50</div>
+    </div>
+  `,
+  "43": `
+    <div class="espelho-item">
+        <input type="checkbox" value="0.10" onchange="window.calcNota()">
+        <div class="espelho-txt">Exemplo Exame 43...</div>
+        <div class="espelho-pt">0,10</div>
+    </div>
+  `
 };
