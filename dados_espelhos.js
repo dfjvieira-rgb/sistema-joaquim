1e9267db011f5e2a61c60384a595ce01ffb03bd0
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-25 21:40:35 -0300

    Add files via upload

---
+++ dados_espelhos.js
@@ -0,0 +1,6 @@
+// Arquivo: dados_espelhos.js
+window.MEUS_ESPELHOS = {
+  "44": `... todo o HTML do 44 que já fizemos ...`,
+  "43": `... o HTML do 43 que você vai criar ...`,
+  "42": `... e assim por diante ...`
+};
