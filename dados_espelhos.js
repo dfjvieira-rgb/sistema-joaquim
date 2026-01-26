4b51093a63a165c9ceb69bbcc38309d5d49b8f5f
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-25 23:33:30 -0300

    Update dados_espelhos.js

--- dados_espelhos.js
+++ dados_espelhos.js
@@ -2,22 +2,67 @@ window.MEUS_ESPELHOS = {
     "44": `
         <div class="nota-floating">ESPELHO DE CORREÇÃO - EXAME 44</div>
         
+        <div class="espelho-group-title">1. ESTRUTURA E QUALIFICAÇÃO (0,50)</div>
         <div class="espelho-item">
             <input type="checkbox" value="0.10" onchange="window.calcNota()">
             <div class="espelho-txt">Endereçamento ao Juízo da ... Vara do Trabalho de ...</div>
             <div class="espelho-pt">0.10</div>
         </div>
-
         <div class="espelho-item">
             <input type="checkbox" value="0.40" onchange="window.calcNota()">
             <div class="espelho-txt">Qualificação das partes e indicação do Art. 840, § 1º, da CLT</div>
             <div class="espelho-pt">0.40</div>
         </div>
 
+        <div class="espelho-group-title">2. PRELIMINARES E PREJUDICIAIS (1,00)</div>
         <div class="espelho-item">
             <input type="checkbox" value="0.50" onchange="window.calcNota()">
-            <div class="espelho-txt">Tese de Mérito 01 (Exemplo: Verbas Rescisórias)</div>
+            <div class="espelho-txt">Preliminar de Inépcia da Inicial (Art. 330, §1º, CPC)</div>
             <div class="espelho-pt">0.50</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.50" onchange="window.calcNota()">
+            <div class="espelho-txt">Prejudicial de Prescrição Quinquenal (Art. 7º, XXIX, CF)</div>
+            <div class="espelho-pt">0.50</div>
+        </div>
+
+        <div class="espelho-group-title">3. MÉRITO (2,50)</div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.60" onchange="window.calcNota()">
+            <div class="espelho-txt">Tese de Mérito 01: Verbas Rescisórias (Fato, Fundamento e Pedido)</div>
+            <div class="espelho-pt">0.60</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.60" onchange="window.calcNota()">
+            <div class="espelho-txt">Tese de Mérito 02: Horas Extras / Adicionais</div>
+            <div class="espelho-pt">0.60</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.60" onchange="window.calcNota()">
+            <div class="espelho-txt">Tese de Mérito 03: Danos Morais ou Equiparação</div>
+            <div class="espelho-pt">0.60</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.70" onchange="window.calcNota()">
+            <div class="espelho-txt">Tese de Mérito 04: Indicação correta de Artigos/Súmulas</div>
+            <div class="espelho-pt">0.70</div>
+        </div>
+
+        <div class="espelho-group-title">4. REQUERIMENTOS FINAIS (1,00)</div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.40" onchange="window.calcNota()">
+            <div class="espelho-txt">Pedidos de Notificação, Provas e Procedência</div>
+            <div class="espelho-pt">0.40</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.40" onchange="window.calcNota()">
+            <div class="espelho-txt">Honorários de Sucumbência (Art. 791-A da CLT)</div>
+            <div class="espelho-pt">0.40</div>
+        </div>
+        <div class="espelho-item">
+            <input type="checkbox" value="0.20" onchange="window.calcNota()">
+            <div class="espelho-txt">Valor da Causa, Local, Data e Assinatura</div>
+            <div class="espelho-pt">0.20</div>
         </div>
     `
 };
