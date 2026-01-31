a19a93f245b1a576bf5b6a68d69b4f6b5f65e98a
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-31 18:47:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,23 @@
-// firebase-config.js - Conexão Centralizada (Versão Auditada)
+// firebase-config.js - Conexão REAL (Versão Joaquim Elite)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Importante: Certifique-se de que os valores abaixo são os do seu Firebase Console
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY", // <--- Verifique se trocou pelo seu
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    databaseURL: "https://SEU_PROJETO.firebaseio.com",
-    projectId: "SEU_PROJETO",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyAmigODFK8R9c0-fWtagdxLWu9xkODfKYQ",
+    authDomain: "masteroab-db5e1.firebaseapp.com",
+    databaseURL: "https://masteroab-db5e1-default-rtdb.firebaseio.com",
+    projectId: "masteroab-db5e1",
+    storageBucket: "masteroab-db5e1.appspot.com",
+    messagingSenderId: "591632731804",
+    appId: "1:591632731804:web:0b7d3f848418041d841804" 
 };
 
-// Inicializa o Firebase uma única vez
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
 // Exportações Modulares
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 
-console.log("🔥 Firebase: Conexão estabelecida e módulos exportados.");
+console.log("🔥 Firebase: Conexão REAL estabelecida!");
