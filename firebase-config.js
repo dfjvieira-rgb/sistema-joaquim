ef23c42158616425d8d49e0304e601f8c6fe785c
dfjvieira-rgb <dfjvieira@gmail.com>
2026-01-31 18:55:21 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,15 @@
-// firebase-config.js - Conexão REAL (Versão Joaquim Elite)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmigODFK8R9c0-fWtagdxLWu9xkODfKYQ",
+    apiKey: "AIzaSyAmigODFK8R9c0-fWtagdxLWu9xkODfKYQ", // Mantenha a sua chave atual
     authDomain: "masteroab-db5e1.firebaseapp.com",
     databaseURL: "https://masteroab-db5e1-default-rtdb.firebaseio.com",
     projectId: "masteroab-db5e1",
     storageBucket: "masteroab-db5e1.appspot.com",
-    messagingSenderId: "591632731804",
-    appId: "1:591632731804:web:0b7d3f848418041d841804" 
+    messagingSenderId: "620234160245",
+    appId: "1:620234160245:web:d0ef18660600087f9ba03c"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportações Modulares
 export const db = getDatabase(app);
-export const auth = getAuth(app);
-
-console.log("🔥 Firebase: Conexão REAL estabelecida!");
